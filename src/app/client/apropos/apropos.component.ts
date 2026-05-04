7614755a137c7c9ac09eeda1f3710f572fc16ce9
Michée Kolony <micheekolony71@gmail.com>
2026-05-04 17:47:00 +0100

    ajout d'équipe

--- src/app/client/apropos/apropos.component.ts
+++ src/app/client/apropos/apropos.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-apropos',
   templateUrl: './apropos.component.html',
   styleUrl: './apropos.component.css'
 })
-export class AproposComponent {
+export class AproposComponent {ngAfterViewInit(): void {
+  new Splide('#image-carousel', {
+    type: 'loop',
+    perPage: 3, // Desktop (par défaut)
+    autoplay: true,
+    gap: '1rem',
+    breakpoints: {
+      768: {
+        perPage: 2, // Mobile & tablette
+      }
+    }
 
+  }).mount();
+}
 }
